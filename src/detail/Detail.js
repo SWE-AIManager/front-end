f612a36883a4b08316a5ef88436f2d58afce1279
hyn4008 <hyn4008@naver.com>
2022-11-23 22:49:17 +0900

    fourth

--- src/detail/Detail.js
+++ src/detail/Detail.js
@@ -4,17 +4,34 @@ import {
     Text,
     style,
     StyleSheet,
+    Image,
 } from "react-native";
+import YoutubePlayer, {YoutubeIframeRef} from "react-native-youtube-iframe";
 
-export default function Detail(  ){
+const Detail = () => {
+
     return(
         <View style={styles.container}>
-            <Text style={styles.category}>Exercise</Text>
-            <Text style={styles.category}>Meal</Text>
-            <Text style={styles.category}>Cleaning</Text>
+            <Text style={styles.category1}>Exercise</Text>
+            <YoutubePlayer
+                height={300}
+                width={400}
+                play={false}
+                videoId={"Hv5dNa_JqFs"}
+            />
+            <YoutubePlayer
+                height={300}
+                width={400}
+                play={false}
+                videoId={"lKwZ2DU4P-A"}
+            />
+            <Text style={styles.category2}>Meal</Text>
+            <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\chicken.jpg")}/>
+            <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\sushi.jpeg")}/>
+            <Text style={styles.category3}>Cleaning</Text>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -22,11 +39,32 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
     },
 
-    category: {
+    category1: {
         fontSize: "25px",
         height:30,
         color: "#808080",
         marginTop: 50,
         marginLeft: 10,
     },
+
+    category2: {
+        fontSize: "25px",
+        height:30,
+        color: "#808080",
+        marginLeft: 10,
+    },
+
+    category3: {
+        fontSize: "25px",
+        height:30,
+        color: "#808080",
+        marginLeft: 10,
+    },
+
+    video: {
+        width: 200,
+        height: 150,
+    },
 });
+
+export default Detail;
