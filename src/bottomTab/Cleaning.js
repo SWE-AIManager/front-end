e06b2c50837b93bfed76d9f7ea3ebc560fc8a311
hyn4008 <hyn4008@naver.com>
2022-11-26 00:09:22 +0900

    tenth

--- src/bottomTab/Cleaning.js
+++ src/bottomTab/Cleaning.js
@@ -5,11 +5,65 @@ import {
     style,
     StyleSheet,
 } from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 const Cleaning = () => {
     return(
         <View style={styles.container}>
-
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="바닥 청소하기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="빨래하기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="설거지하기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="책상 정리하기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="옷 정리하기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
+            <BouncyCheckbox
+                style={styles.checkbox}
+                size={25}
+                fillColor="#fca503"
+                unfillColor="#fcebca"
+                text="쓰레기 버리기"
+                iconStyle={{ borderColor: "E3E3E3" }}
+                onPress={(isChecked: boolean) => {}}
+            />
         </View>
     );
 };
@@ -18,6 +72,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
+        justifyContent: "center",
+    },
+
+    checkbox: {
+        marginLeft: 90,
+        marginBottom: 50,
     },
 });
 
