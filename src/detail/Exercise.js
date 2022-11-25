1688cd51d42f37287253fd972a1abebae83d38d2
hyn4008 <hyn4008@naver.com>
2022-11-25 11:46:23 +0900

    sixth

--- src/detail/Exercise.js
+++ src/detail/Exercise.js
@@ -4,26 +4,48 @@ import {
     Text,
     style,
     StyleSheet,
+    TouchableOpacity,
 } from "react-native";
 import YoutubePlayer, {YoutubeIframeRef} from "react-native-youtube-iframe";
 
 const Exercise = () => {
-
     return(
         <View style={styles.container}>
             <Text style={styles.category1}>Exercise</Text>
-            <YoutubePlayer
-                height={300}
-                width={400}
-                play={false}
-                videoId={"Hv5dNa_JqFs"}
-            />
-            <YoutubePlayer
-                height={300}
-                width={400}
-                play={false}
-                videoId={"lKwZ2DU4P-A"}
-            />
+            <View style={styles.video}>
+                <YoutubePlayer
+                    height={240}
+                    width={320}
+                    play={false}
+                    videoId={"Hv5dNa_JqFs"}
+                />
+                <View flexDirection="row">
+                    <TouchableOpacity style={styles.hashtag0}>
+                        <Text style={styles.tagText}> #홈트 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.hashtag}>
+                        <Text style={styles.tagText}> #스트레칭 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.hashtag}>
+                        <Text style={styles.tagText}> #요가 </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <YoutubePlayer
+                    height={240}
+                    width={320}
+                    play={false}
+                    videoId={"lKwZ2DU4P-A"}
+                />
+                <View flexDirection="row">
+                    <TouchableOpacity style={styles.hashtag0}>
+                        <Text style={styles.tagText}> #홈트 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.hashtag}>
+                        <Text style={styles.tagText}> #다이어트 </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
@@ -42,9 +64,40 @@ const styles = StyleSheet.create({
         marginLeft: 10,
     },
 
+    videoContainer: {
+        flex: 10,
+        backgroundColor: "#fff",
+    },
+
     video: {
-        width: 200,
-        height: 150,
+        marginTop: 20,
+        marginLeft: 33,
+    },
+
+    hashtag0: {
+        width: 70,
+        borderRadius: 25,
+        height: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 5,
+        marginLeft: 3,
+        backgroundColor: "#D3D3D3",
+    },
+
+    hashtag: {
+        width: 70,
+        borderRadius: 25,
+        height: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 5,
+        marginLeft: 13,
+        backgroundColor: "#D3D3D3",
+    },
+
+    tagText: {
+        color: "#808080",
     },
 });
 
--- src/detail/Meal.js
+++ src/detail/Meal.js
@@ -11,8 +11,12 @@ const Meal = () => {
     return(
         <View style={styles.container}>
             <Text style={styles.category2}>Meal</Text>
-            <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\chicken.jpg")}/>
-            <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\sushi.jpeg")}/>
+            <View style={{alignItems: "center"}}>
+                <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\chicken.jpg")}/>
+                <Text style={styles.text}> ▲ Chicken </Text>
+                <Image style={styles.image} source={require("C:\\Users\\nyung\\AIManager\\assets\\sushi.jpeg")}/>
+                <Text style={styles.text}> ▲ Sushi </Text>
+            </View>
         </View>
     );
 };
@@ -25,11 +29,24 @@ const styles = StyleSheet.create({
 
     category2: {
         fontSize: "25px",
-        height:30,
+        height: 30,
         marginTop: 50,
         color: "#808080",
         marginLeft: 10,
     },
+
+    image: {
+        width: 300,
+        height: 200,
+        marginTop:20,
+    },
+
+    name: {
+        fontSize: "15px",
+        height: 20,
+        marginTop: 15,
+        color: "#808080",
+    }
 });
 
 export default Meal;
