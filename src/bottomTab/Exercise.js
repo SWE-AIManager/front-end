7d4145c318f874e735fcaf4997c9ce04943c4d96
lemonmontart <zizizic103@naver.com>
2022-12-16 12:55:04 +0900

    update

--- src/bottomTab/Exercise.js
+++ src/bottomTab/Exercise.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     View,
     ScrollView,
     Text,
+    TextInput,
     style,
     StyleSheet,
     TouchableOpacity,
@@ -10,8 +11,38 @@ import {
 import YoutubePlayer, {YoutubeIframeRef} from "react-native-youtube-iframe";
 
 const Exercise = () => {
+    const [exerciseCycle, setExerciseCycle] = useState(0);
+    const confirm = async(  ) => {
+        await axios.post("http://10.0.2.2:8000/app/goals/", {
+            goal_id: "quu",
+            meal: 0,
+            exercise: 0,
+            clean: exerciseCycle,
+        })
+        .then(function(response) {
+            if(response.status == 201, 302, 304) {  //가능하면 400, 500번대 응답 빼고는 허용 그런식으로 하는게 좋다고 함
+                Alert.alert("Exercise Cycle Saved");
+            }
+        }).catch(function(error){
+            console.log(JSON.stringify(error.response));
+        })
+    };
+
     return(
         <ScrollView style={styles.container}>
+            <View style={styles.enterExerciseCycle}>
+                <Text style={styles.formText}> 목표 운동 주기 </Text>
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholderTextColor="#D3D3D3"
+                        onChangeText={(exerciseCycle) => setExerciseCycle(exerciseCycle)}
+                    />
+                </View>
+                <TouchableOpacity style={styles.confirmBtn} onPress={confirm}>
+                    <Text style={styles.confirmText}> ENTER </Text>
+                </TouchableOpacity>
+            </View>
             <View style={styles.video}>
                 <View flexDirection="row">
                     <TouchableOpacity style={styles.hashtag}>
@@ -72,6 +103,51 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
+    },
+
+    enterExerciseCycle: {
+        flexDirection : "row",
+        alignItems: "center",
+        marginTop: 50,
+        marginBottom: 50,
+    },
+
+    formText: {
+        marginLeft: 90,
+        fontSize: 17,
+        fontWeight: "bold",
+        color: "#606060"
+    },
+
+    TextInput: {
+        height: 30,
+        flex: 1,
+        padding:5,
+        marginLeft: 20,
+    },
+
+    inputView: {
+        backgroundColor: "#fcebca",
+        borderRadius: 10,
+        width: "15%",
+        height: 30,
+        marginLeft: 5,
+    },
+
+    confirmBtn: {
+        width: "15%",
+        borderRadius: 10,
+        height: 30,
+        marginLeft: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fca503",
+    },
+
+    confirmText: {
+        fontSize: 13,
+        fontWeight: "bold",
+        color: "#606060"
     },
 
     videoContainer: {
--- src/bottomTab/Meal.js
+++ src/bottomTab/Meal.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     View,
     ScrollView,
     Text,
+    TextInput,
+    TouchableOpacity,
     style,
     StyleSheet,
     Image,
@@ -10,8 +12,39 @@ import {
 
 
 const Meal = () => {
+    const [mealCycle, setMealCycle] = useState(0);
+    const confirm = async(  ) => {
+        await axios.post("http://10.0.2.2:8000/app/goals/", {
+            goal_id: "quu",
+            meal: 0,
+            exercise: 0,
+            clean: mealCycle,
+        })
+        .then(function(response) {
+            if(response.status == 201, 302, 304) {  //가능하면 400, 500번대 응답 빼고는 허용 그런식으로 하는게 좋다고 함
+                Alert.alert("Meal Cycle Saved");
+            }
+        }).catch(function(error){
+            console.log(JSON.stringify(error.response));
+        })
+    };
+
     return(
         <ScrollView style={styles.container}>
+            <View style={styles.enterMealCycle}>
+                <Text style={styles.formText}> 목표 하루 식사 횟수 </Text>
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholderTextColor="#D3D3D3"
+                        onChangeText={(mealCycle) => setMealCycle(mealCycle)}
+                    />
+                </View>
+                <TouchableOpacity style={styles.confirmBtn} onPress={confirm}>
+                    <Text style={styles.confirmText}> ENTER </Text>
+                </TouchableOpacity>
+            </View>
+
             <View style={{alignItems: "center"}}>
                 <Image style={styles.image} source={require("C:\\Users\\ziziz\\Jansori\\assets\\ttoekbokki.jpg")}/>
                 <Text style={styles.text}> ▲ 떡볶이 </Text>
@@ -35,6 +68,52 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
     },
+
+    enterMealCycle: {
+        flexDirection : "row",
+        alignItems: "center",
+        marginTop: 50,
+        marginBottom: 50,
+    },
+
+    formText: {
+        marginLeft: 90,
+        fontSize: 17,
+        fontWeight: "bold",
+        color: "#606060"
+    },
+
+    TextInput: {
+        height: 30,
+        flex: 1,
+        padding:5,
+        marginLeft: 20,
+    },
+
+    inputView: {
+        backgroundColor: "#fcebca",
+        borderRadius: 10,
+        width: "15%",
+        height: 30,
+        marginLeft: 5,
+    },
+
+    confirmBtn: {
+        width: "15%",
+        borderRadius: 10,
+        height: 30,
+        marginLeft: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fca503",
+    },
+
+    confirmText: {
+        fontSize: 13,
+        fontWeight: "bold",
+        color: "#606060"
+    },
+
 
     image: {
         width: 300,
